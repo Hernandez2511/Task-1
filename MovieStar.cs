b1dd8a9e6d30b562d6c1a14e1e29d24aa146620d
Hernandez2511 <103345754+Hernandez2511@users.noreply.github.com>
2022-04-10 00:17:31 -0300

    Add files via upload

---
+++ MovieStar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+namespace Ejercicio_1
+{
+
+    //MoviStar class
+    public class MovieStar
+    {
+        //variables to store data
+       
+       
+        public DateTime dateofbirth { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string sex { get; set; }
+        public string nationality { get; set; }
+
+       
+        /// Calculate Age 
+            public int calcAge() 
+       {
+          
+            int daynow = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int dob = int.Parse(dateofbirth.ToString("yyyyMMdd"));
+
+            int ans= (daynow - dob) / 10000;
+            return ans;
+
+        }
+
+
+
+    }
+}
---
+++ MovieStarDriver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Ejercicio_1
+{
+    internal class MovieStarDriver
+    {
+        static void Main(string[] args)
+        {
+            // used StreamReader to read file content
+            StreamReader s = new StreamReader(@"C:\input.txt");
+            string jsonString = s.ReadToEnd();
+
+            // objects list of MovieStar class
+            List<MovieStar> objs = JsonConvert.DeserializeObject<List<MovieStar>>(jsonString);
+
+            // iterate through the list of objects and display results
+            foreach (MovieStar star in objs)
+            {
+                Console.WriteLine(star.name + " " + star.surname);
+                Console.WriteLine(star.sex);
+                Console.WriteLine(star.nationality);
+                Console.WriteLine(star.calcAge()+ " years old");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
